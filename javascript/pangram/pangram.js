c948d9b8318d03043b3bb8b27f3ea5ea360e089b
nisozakuto <nisozakuto@gmail.com>
2020-12-26 23:48:51 -0500

    Answered

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -3,6 +3,42 @@
 // convenience to get you started writing code faster.
 //
 
-export const isPangram = () => {
-  throw new Error('Remove this statement and implement this function');
+export const isPangram = (input) => {
+  if (input.length < 26) return false;
+
+  // let isPangram = true;
+  const alphabet = [
+    "q",
+    "w",
+    "e",
+    "r",
+    "t",
+    "y",
+    "u",
+    "i",
+    "o",
+    "p",
+    "l",
+    "k",
+    "j",
+    "h",
+    "g",
+    "f",
+    "d",
+    "s",
+    "a",
+    "z",
+    "x",
+    "c",
+    "v",
+    "b",
+    "n",
+    "m",
+  ];
+  let lower = input.toLowerCase();
+
+  for (let i = 0; i < alphabet.length; i++) {
+    if (lower.includes(alphabet[i]) == false) return false;
+  }
+  return true;
 };
